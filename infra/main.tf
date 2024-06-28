096ad3f80ceb078375e55bade2e25d71330f57e1
sahiru <sahiruwrcg@gmail.com>
2024-06-29 03:42:40 +0530

    chore: Update terraform backend configuration for storing state in S3 bucket

--- infra/main.tf
+++ infra/main.tf
@@ -1,19 +1,21 @@
 provider "aws" {
-  region = "ap-south-1" # Change to your preferred region
+  region = "ap-south-1" 
 }
 
 data "aws_vpc" "main" {
   id = "vpc-07e22bd2ef29ffa46"
 }
 
-# resource "aws_subnet" "subnet" {
-#   vpc_id            = aws_vpc.main.id
-#   cidr_block        = "10.0.1.0/24"
-#   availability_zone = "ap-south-1a"
-# }
-
 data "aws_subnet" "subnet" {
     id = "subnet-0f124cf04da93d0a6"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "tf-state-f-s"
+    key            = "sanda-terraform.tfstate"
+    region         = "eu-north-1"
+  }
 }
 
 
